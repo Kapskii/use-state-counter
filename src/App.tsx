bb1a2fc7346be6168172713119383b1b62c68e5c
Kapskii <kapskij20@gmail.com>
2023-09-13 14:01:02 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [a, setA] = useState(1)
+  const onClickHandler = () => {
+    setA(++a);
+  }
+  const onClickClear = () => {
+    setA(0);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{a}</h1>
+      <button onClick={onClickHandler}>click</button>
+      <button onClick={onClickClear}>clear</button>
     </div>
   );
 }
